425c68e409ab1d8e0f8e69d3c70f43b75a2408c2
zrjohnson3 <zrjohnson3@gmail.com>
2023-11-15 15:15:09 -0500

    Implemented PokemonGrid and PokemonCard components

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,12 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ThemeProvider attribute='class' defaultTheme='dark'>
-          {children}
+          <main className="flex min-h-screen flex-col items-center p-24">
+            <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+              <h2 className='text-2xl text-bold'>Pokemon Finder</h2>
+            </div>
+            {children}
+          </main>
         </ThemeProvider>
       </body>
     </html>
