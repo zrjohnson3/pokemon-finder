c6ca8bf968d35e3d1732df3cba4a11e368894023
zrjohnson3 <zrjohnson3@gmail.com>
2023-11-16 10:19:35 -0500

    Added a filterable PokemonList

--- components/PokemonGrid.tsx
+++ components/PokemonGrid.tsx
@@ -6,18 +6,28 @@ import { PokemonCard } from "./PokemonCard";
 
 // <PokemonGrid pokemonList={data} />
 interface PokemonGridProps {
-    // pokemonList: any;
+    pokemonList: any;
 
-    pokemonList: [
-        name: string,
-        url: string
-    ];
+    // pokemonList: [
+    //     name: string,
+    //     url: string
+    // ];
 };
 
 export function PokemonGrid({ pokemonList }: PokemonGridProps) {
     const [searchText, setSearchText] = useState("");
 
     console.log(pokemonList);
+    // Filter the text
+    const searchFilter = (pokemonList: any) => {
+        return pokemonList.filter((pokemon: any) => {
+            return pokemon.name.toLowerCase().includes(searchText.toLowerCase());
+        })
+    }
+    // save the filtered array of objects
+    const filteredPokemonList = searchFilter(pokemonList);
+
+    // show the filtered array of objects
 
     return (
         <>
@@ -36,11 +46,16 @@ export function PokemonGrid({ pokemonList }: PokemonGridProps) {
                 </div>
                 <h3 className="text-3xl pt-12 pb-6 text-center">Pokemon Collection</h3>
             </div>
-            <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+            <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-3 lg:text-left">
+                {filteredPokemonList.map((pokemon: any) => {
+                    return (
+                        <PokemonCard name={pokemon.name} />
+                    )
+                })}
+                {/* <PokemonCard name="Pikachu" />
                 <PokemonCard name="Pikachu" />
                 <PokemonCard name="Pikachu" />
-                <PokemonCard name="Pikachu" />
-                <PokemonCard name="Pikachu" />
+                <PokemonCard name="Pikachu" /> */}
                 {/* <PokemonCard />
                 <PokemonCard /> */}
 
