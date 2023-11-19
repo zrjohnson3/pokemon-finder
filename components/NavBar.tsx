01f0ae2a1ebf23805d874a4e89c477b995e201c6
zrjohnson3 <zrjohnson3@gmail.com>
2023-11-19 09:11:05 -0500

    Added link to go to random pokemon page

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,26 +1,65 @@
 "use client";
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { AiFillBug } from 'react-icons/ai'
 import { SiPokemon } from "react-icons/si"
 import { CgPokemon } from "react-icons/cg";
 import classNames from 'classnames'; // https://www.npmjs.com/package/classnames
+import { getRandomInt, getRandomPokemonName } from '@/lib/utils';
+import { getPokemon, getPokemonList } from '@/lib/pokemonApi';
+import { useRouter } from 'next/navigation';
+
+// blue #0075BE and yellow #FFCC00, pokemon colors
+
 
 const NavBar = () => {
+
+    const [randomPokemon, setRandomPokemon] = useState('');
+
+    const router = useRouter();
+
+    // useEffect(() => {
+    //     getRandomPokemonName().then((pokemonName) => {
+    //         setRandomPokemon(pokemonName);
+    //     }).catch((error) => {
+    //         console.error("Error fetchin random Pokemon name: ", error);
+    //     })
+    // }, [onclick, setRandomPokemon]);
+
+    const handleRandomPokemonClick = async (e: any) => {
+        e.preventDefault();
+        try {
+            const newName = await getRandomPokemonName();
+            setRandomPokemon(newName);
+            router.push(`/${newName}`);
+        }
+        catch (error) {
+            console.error("Error fetching random Pokemon name: ", error);
+        }
+    }
 
     const currentPath = usePathname();
     console.log(currentPath);
 
     const links = [
         {
-            id: 0, label: <div className="flex flex-row flex-wrap justify-between items-center"><CgPokemon className="w-6 h-6" />
-                <span>Pokemon Finder</span></div>, href: '/'
+            id: 0,
+            label: <div className="flex flex-row flex-wrap justify-between items-center"><CgPokemon className="w-6 h-6" />
+                <span>Pokemon Finder</span></div>,
+            href: '/'
         },
-        { id: 1, label: 'pokemon', href: '/pokemon' },
+        {
+            id: 1,
+            label: <div onClick={handleRandomPokemonClick}>Random Pokemon</div>,
+            href: `/${randomPokemon} `
+
+        },
     ];
+
+
     return (
-        <nav className='flex space-x-6 border-b bg-gray-800 mb-5 px-5 h-14 items-center'>
+        <nav className='flex space-x-6 border-b bg-yellow-400 mb-5 px-5 h-14 items-center'>
             {/* <Link href={"/"} className="flex items-center ">
                 <CgPokemon className="w-6 h-6" />
                 <span className="flex-shrink-0">Pokemon Finder</span>
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,18 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
- 
+import { getPokemonNames } from "./pokemonApi";
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export async function getRandomPokemonName() {
+  const pokemonNames: [] = await getPokemonNames();
+  const randomIndex = Math.floor(Math.random() * pokemonNames.length)
+  console.log("randomIndex", pokemonNames[randomIndex])
+  return pokemonNames[randomIndex];
+}
+
+export function getRandomInt(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
